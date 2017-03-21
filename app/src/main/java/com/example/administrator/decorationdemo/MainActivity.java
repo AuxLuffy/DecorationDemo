61e9e96d1dbd9ea450f18c6debdfc29c0c8ef81d
sunzhangfei <928256819@qq.com>
2017-03-21 19:40:59 +0800

    测试4.4之前的全屏情况

--- app/src/main/java/com/example/administrator/decorationdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/decorationdemo/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.administrator.decorationdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,10 +14,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setFullDisp();
+        setFullDisp();
 //        requestWindowFeature(Window.FEATURE_NO_TITLE);//这个必须在setContentView之前调用
         setContentView(R.layout.activity_main);
-        hideActionBar();
 
         tv = (TextView) findViewById(R.id.tv);
         btn = (Button) findViewById(R.id.btn);
@@ -33,20 +31,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (/*Build.VERSION.SDK_INT < 16*/true) {
             //方法一：使用WindowManager设置
             //设置了WindowManager的flag，将一直保持该flag的效果，除非手动重置了flag。
-//            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
+//                    | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+//                    | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+//                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+            hide();
+
 
             //方法二：使用activity可见的view设置
             //一旦UI Flag被清除（比如跳转到另外的activity），你需要重新设置UI flag来隐藏system bar。
-            View decorView = getWindow().getDecorView();
-            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;//api16以后
-            decorView.setSystemUiVisibility(uiOptions);
+//            View decorView = getWindow().getDecorView();
+//            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LOW_PROFILE;//api16以后
+//            decorView.setSystemUiVisibility(uiOptions);
             // Remember that you should never show the action bar if the
             // status bar is hidden, so hide that too if necessary.
             hideActionBar();
-
         }
     }
+
+    private void hide() {
+        View decorView = getWindow().getDecorView();
+        int option = View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                    | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE//系统ui会有淡黑色背景
+                ;
+        decorView.setSystemUiVisibility(option);
+    }
+
+    private void cancelHide() {
+        View decorView = getWindow().getDecorView();
+        int option = /*View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                |*/ View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+//                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        decorView.setSystemUiVisibility(option);
+    }
+
 
     /**
      * 隐藏ActionBar
@@ -57,7 +81,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        ActionBar actionBar = getSupportActionBar();//高版本可以换成 ActionBar actionBar = getActionBar();  
 //        actionBar.setDisplayShowTitleEnabled(false);
 //        actionBar.setDisplayShowHomeEnabled(false);
-        //方法2：不会保留tab标签 
+        //方法2：不会保留tab标签，整体隐藏
         getSupportActionBar().hide();
     }
 
@@ -65,10 +89,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.tv:
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                hide();
                 break;
             case R.id.btn:
-                getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//                getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                cancelHide();
                 break;
         }
     }
